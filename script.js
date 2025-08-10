792f6fd99385c9e2835d13374f15ae305f79fd82
rjhalloran <ryan.halloran@gmail.com>
2025-08-10 21:34:38 +0100

    Correct resizing issue and add colour drawing

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let gridSquares = 16;
 
 function createGrid(sideSquares) {
-    const container = document.querySelector("#container")
-    
-    for(let i = 1; i < sideSquares + 1; i++) {
+    const container = document.querySelector("#container");
+
+    const grid = document.createElement('div');
+    grid.className = "grid";
+    grid.id = "grid";
+
+    for (let i = 1; i < sideSquares + 1; i++) {
         let row = document.createElement('div');
         row.className = "row";
         row.id = "row" + i
@@ -15,35 +19,57 @@ function createGrid(sideSquares) {
             row.appendChild(box);
         }
 
-        container.appendChild(row)
+        grid.appendChild(row);
+    };
+
+    container.appendChild(grid);
+
+    const boxes = document.querySelectorAll('.box')
+
+    function changeBackground() {
+        const blackMode = document.querySelector('#black');
+        const colourMode = document.querySelector('#colour');
+        
+        if (blackMode.checked) {
+            this.style.backgroundColor = "black";
+        } else if (colourMode.checked) {
+            const r = Math.floor(Math.random()*256);
+            const g = Math.floor(Math.random()*256);
+            const b = Math.floor(Math.random()*256);
+            console.log(r, g, b);
+            this.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+        };
+    };
+        
+    boxes.forEach(box => box.addEventListener('mouseover', changeBackground))
+
+    const clear = document.querySelector('#clear')
+
+    function resetBackground(object) {
+        object.style.backgroundColor = "lightgray";
     }
-}
+
+    clear.addEventListener('click', () => {
+        boxes.forEach(box => resetBackground(box));
+    });
+
+};
 
 createGrid(gridSquares)
-
-const boxes = document.querySelectorAll('.box')
-
-function changeBackground() {
-    this.style.backgroundColor = "black";
-}
-
-boxes.forEach(box => box.addEventListener('mouseover', changeBackground))
-
-const clear = document.querySelector('#clear')
-
-function resetBackground(object) {
-    object.style.backgroundColor = "lightgray";
-}
-
-clear.addEventListener('click', () => {
-    boxes.forEach(box => resetBackground(box));
-});
 
 
 const resize = document.querySelector('#resize');
 
 resize.addEventListener('click', () => {
-    let updatedGridSquares = document.querySelector("#new-size").value;
+    let updatedGridSquares = Math.floor(document.querySelector("#new-size").value);
     console.log(updatedGridSquares);
+
+    const container = document.querySelector("#container");
+    
+    const grid = document.querySelector('#grid');
+
+    if (container.contains(grid)) {
+        container.removeChild(grid);
+    }
     createGrid(updatedGridSquares);
 });
